8758181264725c6dc139a4f16daffa3561cf4a69
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-03-29 13:39:53 +0300

    Proje içerisinde kullanılacak olan eventlar tanımlandı.

---
+++ Shared/OrderEvents/OrderCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using Shared.Messages;
+
+namespace Shared.OrderEvents
+{
+    //CorrelatedBy interface'i ile event'ın hangi saga instance ile ilişkilendirileceğini belirtiyoruz.
+    public class OrderCreatedEvent : CorrelatedBy<Guid>
+    {
+        public OrderCreatedEvent(Guid correlationId)
+        {
+            CorrelationId = correlationId;
+        }
+        public Guid CorrelationId { get; }
+        public List<OrderItemMessage> OrderItems { get; set; }
+    }
+}
