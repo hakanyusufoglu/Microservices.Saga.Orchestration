baabea5227ad551d833232c065ba32beec4f0385
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-03-29 08:40:54 +0300

    StateMachine projesine masstransit entegrasyonu tamamlandı.

--- SagaStateMachine/Program.cs
+++ SagaStateMachine/Program.cs
@@ -1,5 +1,13 @@
+using MassTransit;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.UsingRabbitMq((context, _configure) =>
+    {
+        _configure.Host(builder.Configuration["RabbitMq"]);
+    });
+});
 
 var host = builder.Build();
 host.Run();
