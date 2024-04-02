51394934a1c68d58478255984eec9b7f7129571b
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-04-02 10:12:21 +0300

    Payment.Api için PaymentStartedEventConsumer'ı oluşturuldu. LaunchBrowser false olarak gerçekleştirildi.

---
+++ Payment.Api/Consumers/PaymentStartedEventConsumer.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using Shared.PaymentEvents;
+using Shared.Settings;
+
+namespace Payment.Api.Consumers
+{
+    public class PaymentStartedEventConsumer(ISendEndpointProvider sendEndpointProvider) : IConsumer<PaymentStartedEvent>
+    {
+        public async Task Consume(ConsumeContext<PaymentStartedEvent> context)
+        {
+            var sendEndPoint = await sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMqSettings.StateMachineQueue}"));
+            // işlem başarılıymış gibi davranıyoruz ve PaymentCompletedEvent oluşturuyoruz. 
+            if (true)
+            {
+                PaymentCompletedEvent paymentCompletedEvent = new(context.Message.CorrelationId)
+                {
+                };
+
+                await sendEndpointProvider.Send(paymentCompletedEvent);
+            }
+            else
+            {
+                PaymentFailedEvent paymentFailedEvent = new(context.Message.CorrelationId)
+                {
+                    Message = "Payment failed",
+                    OrderItems = context.Message.OrderItems
+                };
+
+                await sendEndpointProvider.Send(paymentFailedEvent);
+
+            }
+        }
+    }
+}
