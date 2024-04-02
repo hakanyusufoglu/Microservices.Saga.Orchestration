1594ace08f6461e4e319a6a3781e91a20a3edea8
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-04-02 09:51:23 +0300

    Order.Api için consumers oluşturuldu

---
+++ Order.Api/Consumers/OrderCompletedEventConsumer.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using Order.Api.Contexts;
+using Shared.OrderEvents;
+
+namespace Order.Api.Consumers
+{
+    public class OrderCompletedEventConsumer(OrderDbContexts orderDbContext) : IConsumer<OrderCompletedEvent>
+    {
+        public async Task Consume(ConsumeContext<OrderCompletedEvent> context)
+        {
+            Order.Api.Models.Order order = await orderDbContext.Orders.FindAsync(context.Message.OrderId);
+            if(order != null)
+            {
+                order.OrderStatus = Enums.OrderStatus.Completed;
+                await orderDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Order.Api/Consumers/OrderFailedEventConsumer.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using Order.Api.Contexts;
+using Shared.OrderEvents;
+
+namespace Order.Api.Consumers
+{
+    public class OrderFailedEventConsumer(OrderDbContexts orderDbContext) : IConsumer<OrderFailedEvent>
+    {
+        public async Task Consume(ConsumeContext<OrderFailedEvent> context)
+        {
+            Order.Api.Models.Order order = await orderDbContext.Orders.FindAsync(context.Message.OrderId);
+            if (order != null)
+            {
+                order.OrderStatus = Enums.OrderStatus.Fail;
+                await orderDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
